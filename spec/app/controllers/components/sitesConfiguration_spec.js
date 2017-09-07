5ff8d4f11e56ea63f7b3c0009f3e187a63801813
Christy Thomas <christy.thomas@salesforce.com>
2017-09-07 11:08:21 -0400

    fix for running all tests

--- spec/app/controllers/components/sitesConfiguration_spec.js
+++ spec/app/controllers/components/sitesConfiguration_spec.js
@@ -1,160 +1,162 @@
 var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
-describe('app/controllers/components/sitesConfiguration.js', function() {
+describe('app/controllers/components/sitesConfiguration.js', function () {
+    var controllerUnderTest;
+    beforeAll(function () {
+        var stub = {
+            logging: sinon.stub().returns({
+                info: sinon.stub()
+            }),
+            displaySiteDropDown: true
+        };
 
-    var stub = {
-        logging: sinon.stub().returns({
-            info: sinon.stub()
-        }),
-        displaySiteDropDown: true
-    };
+        $ = require('tiunit/mockcontroller').createControllerMock('app/controllers/components/sitesConfiguration.js');
 
-    $ = require('tiunit/mockcontroller').createControllerMock('app/controllers/components/sitesConfiguration.js');
+        $ = _.extend($, {
+            '__controllerPath': 'path',
+            stopListening: sinon.stub(),
+            destroy: sinon.stub(),
+            trigger: sinon.stub(),
+            site_dropdown_view: {
+                init: sinon.stub(),
+                updateSiteSelectedItem: sinon.stub(),
+                deinit: sinon.stub(),
+            },
+            language_dropdown_view: {
+                init: sinon.stub(),
+                deinit: sinon.stub(),
+                enableOrDisableDropdown: sinon.stub(),
+                updateLanguageSelectedItem: sinon.stub(),
+                populateLanguages: sinon.stub()
+            },
+            country_dropdown_view: {
+                init: sinon.stub(),
+                deinit: sinon.stub(),
+                initializeCountries: sinon.stub(),
+                updateCountrySelectedItem: sinon.stub(),
+                enableOrDisableDropdown: sinon.stub()
+            },
+            site_label: {
+                hide: sinon.stub(),
+                setText: sinon.stub(),
+                setHeight: sinon.stub()
+            },
+            country_label: {
+                setText: sinon.stub()
+            },
+            language_label: {
+                setText: sinon.stub()
+            },
+            site_config: {
+                setHeight: sinon.stub()
+            }
+        });
+        // console.dir($);
 
-    $ = _.extend($, {
-        '__controllerPath': 'path',
-        stopListening: sinon.stub(),
-        destroy: sinon.stub(),
-        trigger: sinon.stub(),
-        site_dropdown_view: {
-            init: sinon.stub(),
-            updateSiteSelectedItem: sinon.stub(),
-            deinit: sinon.stub(),
-        },
-        language_dropdown_view: {
-            init: sinon.stub(),
-            deinit: sinon.stub(),
-            enableOrDisableDropdown: sinon.stub(),
-            updateLanguageSelectedItem: sinon.stub(),
-            populateLanguages: sinon.stub()
-        },
-        country_dropdown_view: {
-            init: sinon.stub(),
-            deinit: sinon.stub(),
-            initializeCountries: sinon.stub(),
-            updateCountrySelectedItem: sinon.stub(),
-            enableOrDisableDropdown: sinon.stub()
-        },
-        site_label: {
-            hide: sinon.stub(),
-            setText: sinon.stub(),
-            setHeight: sinon.stub()
-        },
-        country_label: {
-            setText: sinon.stub()
-        },
-        language_label: {
-            setText: sinon.stub()
-        },
-        site_config: {
-            setHeight: sinon.stub()
-        },
+        Alloy.CFG = Alloy.CFG || {};
+        Alloy.CFG.countrySelected = {};
+        Alloy.CFG.languageSelected = {};
+        Alloy.CFG.siteSelected = {};
 
+        controllerUnderTest = proxyquire('../../../../app/controllers/components/sitesConfiguration.js', stub);
     });
 
-    Alloy.CFG = Alloy.CFG || {};
-    Alloy.CFG.countrySelected = {};
-    Alloy.CFG.languageSelected = {};
-    Alloy.CFG.siteSelected = {};
-
-    var controllerUnderTest = proxyquire('../../../../app/controllers/components/sitesConfiguration.js', stub);
-
-    describe('init with site', function() {
-        beforeAll(function() {
+    describe('init with site', function () {
+        beforeAll(function () {
             controllerUnderTest.init(true);
         })
-        it('should have called $.language_dropdown_view.init', function() {
+        it('should have called $.language_dropdown_view.init', function () {
             expect($.language_dropdown_view.init.called).toEqual(true);
         });
     });
 
-    describe('init no site', function() {
-        beforeAll(function() {
+    describe('init no site', function () {
+        beforeAll(function () {
             controllerUnderTest.init(false);
         })
-        it('should have called $.language_dropdown_view.init', function() {
+        it('should have called $.language_dropdown_view.init', function () {
             expect($.language_dropdown_view.init.called).toEqual(true);
         });
     });
 
-    describe('deinit', function() {
-        beforeAll(function() {
+    describe('deinit', function () {
+        beforeAll(function () {
             controllerUnderTest.deinit(true);
         })
-        it('should have called $.destroy', function() {
+        it('should have called $.destroy', function () {
             expect($.destroy.called).toEqual(true);
         });
     });
 
-    describe('onSiteChange success', function() {
-        beforeAll(function() {
+    describe('onSiteChange success', function () {
+        beforeAll(function () {
             controllerUnderTest.onSiteChange(true);
         })
-        it('should have called $.language_dropdown_view.enableOrDisableDropdown with true', function() {
+        it('should have called $.language_dropdown_view.enableOrDisableDropdown with true', function () {
             expect($.language_dropdown_view.enableOrDisableDropdown.calledWith(true)).toEqual(true);
         });
     });
 
-    describe('onSiteChange failure', function() {
-        beforeAll(function() {
+    describe('onSiteChange failure', function () {
+        beforeAll(function () {
             controllerUnderTest.onSiteChange(false);
         })
-        it('should have called $.language_dropdown_view.enableOrDisableDropdown with false', function() {
+        it('should have called $.language_dropdown_view.enableOrDisableDropdown with false', function () {
             expect($.language_dropdown_view.enableOrDisableDropdown.calledWith(false)).toEqual(true);
         });
     });
 
-    describe('onCountryChange', function() {
-        beforeAll(function() {
+    describe('onCountryChange', function () {
+        beforeAll(function () {
             controllerUnderTest.onCountryChange();
         })
-        it('should have called $.language_dropdown_view.populateLanguages', function() {
+        it('should have called $.language_dropdown_view.populateLanguages', function () {
             expect($.language_dropdown_view.populateLanguages.called).toEqual(true);
         });
     });
 
-    describe('onLanguageChange', function() {
-        beforeAll(function() {
+    describe('onLanguageChange', function () {
+        beforeAll(function () {
             controllerUnderTest.onLanguageChange();
         })
-        it('should have called $.trigger', function() {
+        it('should have called $.trigger', function () {
             expect($.trigger.called).toEqual(true);
         });
     });
 
-    describe('updateSelectedItems with site', function() {
-        beforeAll(function() {
+    describe('updateSelectedItems with site', function () {
+        beforeAll(function () {
             Alloy.CFG.siteSelected = {};
             controllerUnderTest.updateSelectedItems();
         })
-        it('should have called $.site_dropdown_view.updateSiteSelectedItem', function() {
+        it('should have called $.site_dropdown_view.updateSiteSelectedItem', function () {
             expect($.site_dropdown_view.updateSiteSelectedItem.called).toEqual(true);
         });
     });
 
-    describe('updateSelectedItems without site', function() {
-        beforeAll(function() {
+    describe('updateSelectedItems without site', function () {
+        beforeAll(function () {
             displaySiteDropDown = false;
             controllerUnderTest.updateSelectedItems();
         })
-        it('should have called $.site_dropdown_view.updateLanguageSelectedItem', function() {
+        it('should have called $.site_dropdown_view.updateLanguageSelectedItem', function () {
             expect($.language_dropdown_view.updateLanguageSelectedItem.called).toEqual(true);
         });
     });
 
-    describe('updateLabels', function() {
-        beforeAll(function() {
+    describe('updateLabels', function () {
+        beforeAll(function () {
             controllerUnderTest.updateLabels();
         })
-        it('should have called $.country_dropdown_view.initializeCountries', function() {
+        it('should have called $.country_dropdown_view.initializeCountries', function () {
             expect($.country_dropdown_view.initializeCountries.called).toEqual(true);
         });
     });
 
-    describe('handleEmptyLanguageDropdown', function() {
-        beforeAll(function() {
+    describe('handleEmptyLanguageDropdown', function () {
+        beforeAll(function () {
             controllerUnderTest.handleEmptyLanguageDropdown();
         })
-        it('should have called $.trigger', function() {
+        it('should have called $.trigger', function () {
             expect($.trigger.called).toEqual(true);
         });
     });
