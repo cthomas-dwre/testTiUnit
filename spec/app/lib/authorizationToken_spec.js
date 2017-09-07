9f78a988a788bde3ab54c42a032a8c508abba7fb
Christy Thomas <christy.thomas@salesforce.com>
2017-09-07 08:57:27 -0400

    authorizationToken unit test

--- spec/app/lib/authorizationToken_spec.js
+++ spec/app/lib/authorizationToken_spec.js
@@ -8,29 +8,67 @@ describe('app/lib/authorizationToken.js', function() {
             log: sinon.stub(),
             error: sinon.stub(),
             secureLog: sinon.stub()
-        }),
-        displaySiteDropDown: true,
+        })
     };
 
-    var controllerUnderTest = proxyquire('../../../app/lib/authorizationToken.js', stub);
+    var libUnderTest = proxyquire('../../../app/lib/authorizationToken.js', stub);
 
-    var createHTTPClient = sinon.stub().returns({
+    var httpClient = {
         open: sinon.stub(),
         setRequestHeader: sinon.stub(),
         setWithCredentials: sinon.stub(),
-        send: sinon.stub(),
-        responseText: {},
-        access_token: '11',
-        status: 200,
+        responseText: '{"expires_in": 1}',
+        access_token: 'some_token'
+    };
+
+    describe('fetchToken onerror', function() {
+        var promise;
+        beforeAll(function() {
+            var retval = _.extend(httpClient, {
+                send: sinon.stub().callsFake(function() {
+                    this.onerror();
+                }),
+                status: 200
+            });
+            Ti.Network.createHTTPClient = sinon.stub().returns(retval);
+            promise = libUnderTest.fetchToken();
+        })
+        it('should have rejected promise', function() {
+            expect(promise.state()).toEqual('rejected');
+        });
     });
 
-    describe('fetchToken', function() {
+    describe('fetchToken onload bad status', function() {
+        var promise;
         beforeAll(function() {
-            Ti.Network.createHTTPClient = createHTTPClient;
-            controllerUnderTest.fetchToken();
+            var retval = _.extend(httpClient, {
+                send: sinon.stub().callsFake(function() {
+                    this.onload();
+                }),
+                status: 400
+            });
+            Ti.Network.createHTTPClient = sinon.stub().returns(retval);
+            promise = libUnderTest.fetchToken();
         })
-        it('should have called http.send', function() {
-            expect(http.send.called).toEqual(true);
+        it('should have rejected promise', function() {
+            expect(promise.state()).toEqual('rejected');
+        });
+    });
+
+    describe('fetchToken onload', function() {
+        var promise;
+        beforeAll(function() {
+            var retval = _.extend(httpClient, {
+                send: sinon.stub().callsFake(function() {
+                    this.onload();
+                }),
+                status: 200
+            });
+            Ti.Network.createHTTPClient = sinon.stub().returns(retval);
+            promise = libUnderTest.fetchToken();
+        })
+        it('should have resolved promise', function() {
+            expect(promise.state()).toEqual('resolved');
         });
     });
 
